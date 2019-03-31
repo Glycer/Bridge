a25ff746da084e6a5b7739ff01f189046d2b3440
Daniel <danieldmotter@gmail.com>
2019-03-30 18:11:39 -0600

    Chase

---
+++ Under the Bridge/Assets/3D/Monsters/Scripts/EnemyLockOn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLockOn : MonoBehaviour
+{
+    public PlayerFound playerScan;
+    public PlayerMotion player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerScan.foundTarget == true)
+        {
+            transform.LookAt(player.transform.position);
+            transform.Translate(0, 0, 1);
+        }
+    }
+}
---
+++ Under the Bridge/Assets/3D/Monsters/Scripts/PlayerFound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFound : MonoBehaviour
+{
+    public bool foundTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foundTarget = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        foundTarget = true;
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        foundTarget = false;
+    }
+}
